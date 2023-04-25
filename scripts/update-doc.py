5e764d4c147acf0f25c41bb2017efed12428d697
Bruno Botelho <bruno.botelho.br@gmail.com>
2023-04-25 18:34:57 -0300

    Task: Update Documentation

--- scripts/update-doc.py
+++ scripts/update-doc.py
@@ -11,9 +11,10 @@ args = parser.parse_args()
 
 branch = args.branch
 
-
 CURRENT_BRANCH = str(os.system("git rev-parse --abbrev-ref HEAD) > /dev/null"))
+print("Current Branch: " + CURRENT_BRANCH)
 os.system("git checkout " + branch + " > /dev/null")
+print("Current Branch: " + branch)
 os.system("git add * > /dev/null")
 os.system("git commit -m 'Task: Update Documentation' > /dev/null")
 os.system("git push --set-upstream origin " + branch)
