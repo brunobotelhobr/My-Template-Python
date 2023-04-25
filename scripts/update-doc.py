47c85d1916b08153be1495c2a6899c130637cba1
Bruno Botelho <bruno.botelho.br@gmail.com>
2023-04-25 18:36:35 -0300

    Task: Update Documentation

--- scripts/update-doc.py
+++ scripts/update-doc.py
@@ -5,16 +5,18 @@ print("----------------------------------------------------------------")
 print("Updating Documentation, point github sites to the correct branch")
 print("----------------------------------------------------------------")
 
-parser = argparse.ArgumentParser(description="Update Documentation")
-parser.add_argument("branch", type=str, help="Branch to update")
+#Get the tirst argument
+parser = argparse.ArgumentParser()
+parser.add_argument("branch", help="The branch to update the documentation for")
 args = parser.parse_args()
-
 branch = args.branch
 
 CURRENT_BRANCH = str(os.system("git rev-parse --abbrev-ref HEAD) > /dev/null"))
 print("Current Branch: " + CURRENT_BRANCH)
+
 os.system("git checkout " + branch + " > /dev/null")
 print("Current Branch: " + branch)
+
 os.system("git add * > /dev/null")
 os.system("git commit -m 'Task: Update Documentation' > /dev/null")
 os.system("git push --set-upstream origin " + branch)
